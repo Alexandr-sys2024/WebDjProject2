d4c006805cc4a1b52bccdb86c53a6eb8625322fb
Alexandr-sys2024 <time2016slot@gmail.com>
2024-11-26 20:53:32 +0300

    add new code in firsturlspy

--- SashaandDjango/first/views.py
+++ SashaandDjango/first/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
 
+def data(request):
+    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Это третья страница моего проекта на Django</h1>")
+
+
 # Create your views here.
