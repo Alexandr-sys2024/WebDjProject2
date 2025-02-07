d667c4580daa36375c25ec74d798078ff3ddf829
Alexandr-sys2024 <time2016slot@gmail.com>
2025-02-07 10:39:31 +0300

    add new code in settings

--- SashaandDjango/SashaandDjango/settings.py
+++ SashaandDjango/SashaandDjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SashaandDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'SashaandDjango', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
