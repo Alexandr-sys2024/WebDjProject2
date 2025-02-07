ff25d02d05233f3b28973d92d66f483f52c2d838
Alexandr-sys2024 <time2016slot@gmail.com>
2025-02-07 10:41:13 +0300

    add new code in urls i first

--- SashaandDjango/first/urls.py
+++ SashaandDjango/first/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),  # Главная страница
     path('data', views.data, name='data'),
     path('test', views.test, name='test')
 ]
