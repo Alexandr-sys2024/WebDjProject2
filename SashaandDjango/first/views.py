d8a151fb0a2240f1f90db6688f8ef3a115cf229a
Alexandr-sys2024 <time2016slot@gmail.com>
2025-02-07 10:21:42 +0300

    add file

--- SashaandDjango/first/views.py
+++ SashaandDjango/first/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+def data(request):
+    return HttpResponse("Hello, this is the data view!")
